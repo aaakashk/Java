add3d8707fcb266f8a4daf2e90b3ac977d7c7c9e
Akash Kumar <akashyadav112314@gmail.com>
2022-03-27 00:27:37 +0530

    used String for input

---
+++ PCS-408/Week_3/replace_0_1.java
@@ -0,0 +1,17 @@
+import java.util.Scanner;
+import java.lang.StringBuffer;
+
+class Replace{
+    public static void main(String[] args){
+	Scanner scanner = new Scanner(System.in);
+	String num;
+	System.out.print("Enter a number: ");
+	num = scanner.nextLine();
+	StringBuffer sb = new StringBuffer();
+	sb.append(num);
+	for(int i = 0; i < sb.length(); i++){
+	    if(sb.charAt(i) == '0') sb.replace(i, i + 1, "1");
+	}
+	System.out.println(sb);
+    }
+}
