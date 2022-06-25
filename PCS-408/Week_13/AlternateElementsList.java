597285766e564e8ccd23cfcc3bf08b01d325fa8c
Akash Kumar <akashyadav112314@gmail.com>
2022-06-25 22:46:58 +0530

    ArrayList done

---
+++ PCS-408/Week_13/AlternateElementsList.java
@@ -0,0 +1,32 @@
+import java.util.ArrayList;
+import java.util.Iterator;
+import java.util.Scanner;
+
+public class AlternateElementsList {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        System.out.println("Enter the size of list 1 and list 2: ");
+        int m = sc.nextInt();
+        int n = sc.nextInt();
+        ArrayList<Integer> list_1 = new ArrayList<>();
+        ArrayList<Integer> list_2 = new ArrayList<>();
+        System.out.println("Enter elements of list 1: ");
+        for(int i = 0; i < m; i++)
+            list_1.add(sc.nextInt());
+        System.out.println("Enter elements of list 2 :");
+        for(int i = 0; i < n; i++)
+            list_2.add(sc.nextInt());
+        System.out.println(alternate(list_1, list_2));
+    }
+
+    public static ArrayList<Integer> alternate(ArrayList<Integer> list_1, ArrayList<Integer> list_2) {
+        Iterator<Integer> i1 = list_1.iterator();
+        Iterator<Integer> i2 = list_2.iterator();
+        ArrayList<Integer> newList = new ArrayList<>();
+        while(i1.hasNext() || i2.hasNext()) {
+            if(i1.hasNext()) newList.add(i1.next());
+            if(i2.hasNext()) newList.add(i2.next());
+        }
+        return newList;
+    }
+}
---
+++ PCS-408/Week_13/RemoveEvenLengthStrings.java
@@ -0,0 +1,23 @@
+import java.util.ArrayList;
+import java.util.Scanner;
+
+public class RemoveEvenLengthStrings {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        System.out.println("Enter the size of the list: ");
+        int n = sc.nextInt();
+        System.out.println("Enter words for the list: ");
+        ArrayList<String> list = new ArrayList<>();
+        for(int i = 0; i < n; i++) {
+            list.add(sc.next());
+        }
+        for(int i = 0; i < list.size(); i++) {
+            String str = list.get(i);
+            if(str.length() % 2 == 0) {
+                list.remove(i);
+                i--;
+            }
+        }
+        System.out.println(list);
+    }
+}
